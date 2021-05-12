a2d00174cc775725c58f84047d44df17d7bb5dc7
rsm-akotha <akotha@ucsd.edu>
2021-05-12 13:51:36 -0700

    removed .tox and .ipynb_checkpoints online

--- .tox/py38-local/lib/python3.8/site-packages/pydocstyle/__init__.py
+++
@@ -1,5 +0,0 @@
-# Temporary hotfix for flake8-docstrings
-from .checker import ConventionChecker, check
-from .parser import AllError
-from .utils import __version__
-from .violations import Error, conventions
--- .tox/py38/lib/python3.8/site-packages/peopleanalyticsdata/__init__.py
+++
@@ -1,4 +0,0 @@
-from .peopleanalyticsdata import list_sets, charity_donation, employee_survey, health_insurance, job_retention, managers, politics_survey, salespeople, soccer, sociological_data, speed_dating, ugtests, employee_performance, learning, graduates, promotion, recruiting 
-
-__version__ = '0.1.4'
-__author__ = 'Akshay Kotha'
